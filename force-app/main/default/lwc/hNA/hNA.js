ca6a68133414cb785f7457ca1c5cadee1fdc8206
Joshua Dancey <joshuadancey@hotmail.com>
2023-05-24 23:18:47 +0100

    setting up hna object wrapper, creation and saving, interconnecting records for record page

--- force-app/main/default/lwc/hNA/hNA.js
+++ force-app/main/default/lwc/hNA/hNA.js
@@ -1,8 +1,7 @@
 import { getRecord } from "lightning/uiRecordApi";
 import { LightningElement, api, wire } from "lwc";
 
-// eslint-disable-next-line no-unused-vars
-import hNARecord from "c/hNARecord";
+import getServiceUsersHNA from "@salesforce/apex/Service_User.getServiceUsersHNA";
 
 import DATE_OF_BIRTH_FIELD from "@salesforce/schema/Service_User__c.Date_Of_Birth__c";
 import FIRST_NAME_FIELD from "@salesforce/schema/Service_User__c.First_Name__c";
@@ -12,9 +11,8 @@ const fields = [DATE_OF_BIRTH_FIELD, FIRST_NAME_FIELD, LAST_NAME_FIELD];
 
 export default class MyComponent extends LightningElement {
   serviceUserDetailsError;
-  serviceUserDetailsData;
-
-  serviceUser = {};
+  serviceUser;
+  hnaId;
 
   @api recordId;
 
@@ -22,12 +20,27 @@ export default class MyComponent extends LightningElement {
   wireRecords({ error, data }) {
     if (error) this.serviceUserDetailsError = JSON.stringify(error);
     if (data) {
-      console.log(data, "data");
-      this.serviceUserDetailsData = JSON.stringify(data);
+      this.serviceUser = {};
       this.serviceUser.id = data.id;
       this.serviceUser.fName = data.fields.First_Name__c.value;
       this.serviceUser.lName = data.fields.Last_Name__c.value;
       this.serviceUser.dob = data.fields.Date_of_Birth__c?.displayValue;
+      this.getServiceUserHNAMethod(data.id);
     }
+  }
+  async getServiceUserHNAMethod(id) {
+    try {
+      const HNA = await getServiceUsersHNA({
+        serviceUsersId: id
+      });
+
+      this.hnaId = HNA[0].Id;
+    } catch (e) {
+      console.log(e, "My Lovely Error");
+    }
+  }
+  handleHNASuccess(e) {
+    console.log("handling success, you cant handle the truth");
+    this.hnaId = e.detail.id;
   }
 }
--- force-app/main/default/lwc/hNARecord/hNARecord.js
+++ force-app/main/default/lwc/hNARecord/hNARecord.js
@@ -1,10 +1,17 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
+//Drug and Alcohol imports
 import HNADrugAndAlcohol from "@salesforce/schema/HNA_Drug_and_Alcohol__c";
 import CURRENT_SUPPORTS_FIELD from "@salesforce/schema/HNA_Drug_and_Alcohol__c.Current_Supports__c";
 import DRUGS_USED_FIELD from "@salesforce/schema/HNA_Drug_and_Alcohol__c.Drugs_Used__c";
 import FREQUENCY_USE_FIELD from "@salesforce/schema/HNA_Drug_and_Alcohol__c.Frequency_Use__c";
 import HOLISTIC_NEEDS_ASSESSMENT_FIELD from "@salesforce/schema/HNA_Drug_and_Alcohol__c.Holistic_Needs_Assessment__c";
 import SERVICE_USER_FIELD from "@salesforce/schema/HNA_Drug_and_Alcohol__c.Service_User__c";
+
+//housing imports
+import HNAHousing from "@salesforce/schema/HNA_Housing__c";
+import HOLISTIC_NEEDS_ASSESSMENT_FIELD_HOUSING from "@salesforce/schema/HNA_Housing__c.Holistic_Needs_Assessment__c";
+import SERVICE_USER_FIELD_HOUSING from "@salesforce/schema/HNA_Housing__c.Service_User__c";
+import NAME_FIELD_HOUSING from "@salesforce/schema/HNA_Housing__c.Name";
 
 const drugAndAlcoholFields = [
   CURRENT_SUPPORTS_FIELD,
@@ -13,8 +20,53 @@ const drugAndAlcoholFields = [
   HOLISTIC_NEEDS_ASSESSMENT_FIELD,
   SERVICE_USER_FIELD
 ];
+const uniqueDrugAndAlcoholFields = [
+  CURRENT_SUPPORTS_FIELD,
+  DRUGS_USED_FIELD,
+  FREQUENCY_USE_FIELD
+];
+
+const housingFields = [
+  HOLISTIC_NEEDS_ASSESSMENT_FIELD_HOUSING,
+  SERVICE_USER_FIELD_HOUSING,
+  NAME_FIELD_HOUSING
+];
+const uniqueHousingFields = [NAME_FIELD_HOUSING];
 
 export default class HNARecord extends LightningElement {
-  fields = drugAndAlcoholFields;
-  objectApiName = HNADrugAndAlcohol.objectApiName;
+  fields;
+  uniqueFields;
+  objectApiName;
+  myTypeThing;
+  recordPresent = false;
+  accessibleServiceUser;
+
+  @api type;
+  @api serviceUser;
+  @api hnaId;
+
+  connectedCallback() {
+    this.setFieldsAndApiName();
+    this.accessibleServiceUser = JSON.parse(JSON.stringify(this.serviceUser));
+  }
+
+  setFieldsAndApiName() {
+    switch (this.type) {
+      case "drugAndAlcohol":
+        this.fields = drugAndAlcoholFields;
+        this.objectApiName = HNADrugAndAlcohol.objectApiName;
+        this.myTypeThing = this.type;
+        this.uniqueFields = uniqueDrugAndAlcoholFields;
+
+        break;
+      case "house":
+        this.fields = housingFields;
+        this.objectApiName = HNAHousing.objectApiName;
+        this.myTypeThing = this.type;
+        this.uniqueFields = uniqueHousingFields;
+        break;
+      default:
+        break;
+    }
+  }
 }
